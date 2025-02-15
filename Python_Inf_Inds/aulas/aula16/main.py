e4787f35d26b4ad76194135d907f5e9c37a0231e
Anonmixs Amaro Afonso <anonmixs.afonso@gmail.com>
2025-02-15 13:46:26 -0300

    Add calculator

--- Python_Inf_Inds/aulas/aula16/main.py
+++ Python_Inf_Inds/aulas/aula16/main.py
@@ -4,14 +4,29 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.config import Config
 
 buffer = list()
-
 class Calculator(BoxLayout):
     def acumulatevalues(self, text):
         buffer.append(text)
         print(buffer)
     def showinscreen(self, textshow):
-        self.ids['lab'].text = str(textshow)
-
+        self.ids['lab'].text = " ".join(map(str,buffer))
+    def calculate(self):
+        if (len(buffer) > 0):
+            try:
+                self.ids['lab'].text =  str(eval(''.join(buffer)))
+            except Exception as args:
+                self.ids['lab'].text =  'Invalid Operation'
+    def delete (self):
+        if (len(buffer) > 0):
+            sizel = len(buffer)
+            sizeel = len(buffer[0])
+            numel = sizel/sizeel
+            buffer.pop(int(numel-1))
+            self.ids['lab'].text = " ".join(map(str,buffer))
+    def ac (self):
+        if (len(buffer) > 0):
+            buffer.clear()
+            self.ids['lab'].text = '0'
 class BasicCalculatorApp(App):
     def build(self):
         return Calculator()
