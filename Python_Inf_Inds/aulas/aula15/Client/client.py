f0c454162e8f799cf77babc810f3b81c0d868ab3
Anonmixs Amaro Afonso <anonmixs.afonso@gmail.com>
2025-03-12 18:15:31 -0300

    Att files

--- Python_Inf_Inds/aulas/aula15/Client/client.py
+++ Python_Inf_Inds/aulas/aula15/Client/client.py
@@ -17,14 +17,14 @@ class Clientinit:
             print("Welcome to SCADA CLI: \n")
             ch = input("Choose what to do: 1- Read values from registers, 2- Write values to register: ")
             if (int(ch) == 1):
-                typreg = input("Choose: 1- Read coil, 2- Read holding registers, 3- Read holding registers bits: ")
+                typreg = input("Choose: 1- Read holding registers (int), 2- Read holding registers (float), 3- Read holding registers bits: ")
                 if (int(typreg) == 1):
                     addr = input("Address of the coil in the MODBUS table: ")
                     print(f'The value in {addr} is {self.readvalue(int(addr), int(typreg))}. \n')
 
                 if (int(typreg) == 2):
                     addr = input("Address of the holding register in the MODBUS table: ")
-                    print(f'The value in {addr} is {self.readvalue(int(addr), int(typreg))}. \n')
+                    print(f'The value in {addr} is {float(self.readvalue(int(addr), int(typreg)))}. \n')
 
                 if (int(typreg) == 3):
                     addr = input("Address of the holding register in the MODBUS table: ")
@@ -57,20 +57,26 @@ class Clientinit:
         if (int(typ) == 1):
             # decoder = BinaryPayloadDecoder(payload=self.modclient.read_coils(addr, 1), byteorder=Endian.LITTLE)
             # val = decoder.decode_32bit_float()
-            return self.modclient.read_coils(addr, 1)[0]
+            ##Holding
+            return self.modclient.read_holding_registers(addr, 1)[0]
+            ##Floating
         if (int(typ) == 2):
-            # return self.modclient.read_holding_registers(addr, 2)
-            decoder = BinaryPayloadDecoder(payload=self.modclient.read_holding_registers(addr, 2), byteorder=Endian.LITTLE)
+            register = self.modclient.read_holding_registers(addr, 2)
+            decoder = BinaryPayloadDecoder.fromRegisters(register, byteorder=Endian.BIG, wordorder=Endian.LITTLE)
             val = decoder.decode_32bit_float()
+            print(f'Inside {val}.\n')
             return val
 
     def readbitholding (self, addr):
-        decoder = BinaryPayloadDecoder(payload=self.modclient.read_holding_registers(addr, 2), byteorder=Endian.LITTLE)
-        val = decoder.decode_32bit_float()
-        binary = np.binary_repr(np.float16(val).view(np.int16), width=16)
-        listbit = list(str(binary))
-        intlistbit = [int(c) for c in listbit] 
-        return intlistbit
+        # decoder = BinaryPayloadDecoder(payload=self.modclient.read_holding_registers(addr, 2), byteorder=Endian.LITTLE)
+        # val = decoder.decode_32bit_float()
+        # binary = np.binary_repr(np.float16(val).view(np.int16), width=16)
+        # listbit = list(str(binary))
+        # intlistbit = [int(c) for c in listbit] 
+        # return intlistbit
+        register = self.modclient.read_holding_registers(addr, 1)[0]
+        # Convertendo o valor do registrador para uma lista de bits
+        return [int(x) for x in format(register, '016b')]
 
     def writebitholding (self, addr, listbitrec):
         listabit = self.readbitholding(addr)
@@ -81,15 +87,24 @@ class Clientinit:
         for c in range (0, 16, 1):
             a = intlistbit[15-c]*(2**(c)) + a
         
-        self.floatload.add_32bit_float(a)
-        self.payload = self.floatload.to_registers()
+        # self.floatload.add_32bit_float(a)
+        # self.payload = self.floatload.to_registers()
         
-        self.modclient.write_multiple_registers(addr, self.payload)   
+        # self.modclient.write_multiple_registers(addr, self.payload) 
+        self.modclient.write_single_register(addr, a)  
 
     def writevalue (self, addr, typ, val):
-        if (int(typ) == 1):
-            return self.modclient.write_single_coil(addr, val)
-        if (int(typ) == 2):
-            self.floatload.add_32bit_float(val)
-            self.payload = self.floatload.to_registers()
-            return self.modclient.write_multiple_registers(addr, self.payload)       
+        # if (int(typ) == 1):
+        #     return self.modclient.write_single_coil(addr, val)
+        # if (int(typ) == 2):
+            # self.floatload.add_16bit_float(val)
+            # self.payload = self.floatload.to_registers()
+           # return self.modclient.write_multiple_registers(addr, self.payload)       
+
+
+        # 1324 -> Inversor(2)
+        # 1313 -> Velocidade(600)
+        # 1314 -> Rampa ace (100)
+        # 1315 -> Rampa des (100)
+
+        return self.modclient.write_single_register(addr, val)
--- Python_Inf_Inds/aulas/aula15/Client/main.py
+++ Python_Inf_Inds/aulas/aula15/Client/main.py
@@ -1,5 +1,5 @@
 from client import Clientinit
 
-client = Clientinit('localhost', 1502)
+client = Clientinit('10.15.20.25', 502)
 client.ihmcli()
 
